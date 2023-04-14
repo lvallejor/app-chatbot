641fec192a45e51d94ccb4d59ee320d8ecca0223
Luis Vallejo <lvallejo.cl@gmail.com>
2023-04-14 19:02:22 -0400

    nueva

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [content, setContent] = useState("");
+  const [response, setResponse] = useState(null);
+  const [image, setImage] = useState(null);
+
+  const handleInputChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const respuestaApi = await axios.post(
+        `https://api-chatgpt.herokuapp.com/chatbot/?content=${content}`
+      );
+      const responseData = await respuestaApi.data;
+      console.log(responseData);
+      setResponse(responseData.respuesta);
+
+      // Make a second API call to get the image
+      const imagenApi = await axios.post(
+        `https://api-chatgpt.herokuapp.com/imagen/?content=${content}`
+      );
+      const imageData = await imagenApi.data;
+      console.log(imageData.Imagen);
+      setImage(imageData.Imagen);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="content">Como te asisto:</label>
+        <input
+          type="text"
+          id="content"
+          value={content}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Preguntar</button>
+      </form>
+
+      {response && (
+        <div>
+          <p>{response}</p>
+        </div>
+      )}
+
+      {image && (
+        <div>
+          <img src={image} alt="related image" />
+        </div>
+      )}
     </div>
   );
 }
